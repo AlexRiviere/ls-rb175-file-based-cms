9e8a394e80565e66bcc68a8b1897804416b2013d
Alex Riviere <alexanderm.riviere@gmail.com>
2022-06-07 11:09:14 +0000

    up to ch 7

---
+++ test/cms_test.rb
@@ -0,0 +1,54 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.md"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+  
+  def test_file
+    get "/history.txt"
+    
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "Ruby 0.95 released"
+  end
+  
+  def test_document_not_found
+    get "/notafile.ext"
+    
+    assert_equal 302, last_response.status
+    
+    get last_response["Location"]
+    
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+    
+    get "/"
+    refute_includes last_response.body, "notafile.ext does not exist"
+  end
+  
+  def test_md_file
+    get "/about.md"
+    
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
+  end
+end
